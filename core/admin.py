35a93696ac8f2a7eee436e1ef8176d5037bc32c6
mr azad <ali.azadmanjiri2000@gmail.com>
2023-12-22 17:07:30 +0330

    feat: Add CustomUserAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'email', 'is_staff', 'is_superuser', )
+    list_filter = ('is_staff', )
+    search_fields = ("email", )
+    ordering = ("email", )
+    filter_horizontal = ()
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", ),
+            },
+        ),
+    )
